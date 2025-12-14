08c409f1a1f7ca72f12bc06b65f396d340abcee0
LiamJ-D <liam_dyer@outlook.com>
2025-12-14 18:45:31 -0330

    Delete CP3490_Project/CampusManagementSystem directory

--- CP3490_Project/CampusManagementSystem/src/model/Organizer.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Organizer extends User {
-    public Organizer(int id, String name, String email, String password) {
-        super(id, name, email, "Organizer", password);
-    }
-}
--- CP3490_Project/CampusManagementSystem/src/model/Participant.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Participant extends User {
-    public Participant(int id, String name, String email, String password) {
-        super(id, name, email, "Participant", password);
-    }
-}
